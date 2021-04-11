f9733ab09e58bd7d9f3c5a5451cf630c5719bc60
Ákos Levente Kincs <kincsakos23@gmail.com>
2021-04-11 18:46:29 +0200

    Created 2 classes in package 'controller', launch screen loads well

---
+++ src/main/java/main/MyApplication.java
@@ -0,0 +1,21 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/launch.fxml"));
+        primaryStage.setTitle("Tic-Tac-Toe");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setX((Screen.getPrimary().getBounds().getWidth()/2)-300);
+        primaryStage.setY((Screen.getPrimary().getBounds().getHeight()/2)-250);
+        primaryStage.show();
+    }
+}
