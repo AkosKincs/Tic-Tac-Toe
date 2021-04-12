9bb6f23183f01109ad66d2468fe8c26b336c5329
Ákos Levente Kincs <kincsakos23@gmail.com>
2021-04-12 22:30:54 +0200

    Controller class for launching the game has been added

---
+++ src/main/java/controller/LaunchController.java
@@ -0,0 +1,63 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class LaunchController {
+    @FXML
+    private TextField player1TextField;
+
+    @FXML
+    private TextField player2TextField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private Image gameLogo;
+
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private void closeButtonAction(){
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void startAction(ActionEvent actionEvent) throws IOException {
+
+        if (player1TextField.getText().isEmpty()) {
+            errorLabel.setText("Please enter a name for Player1!");
+        }
+        else if(player2TextField.getText().isEmpty()){
+            errorLabel.setText("Please enter a name for Player2!");
+        }
+        else {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
+            Parent root = fxmlLoader.load();
+            //fxmlLoader.<GameController>getController().initializeData(player1TextField.getText(), player2TextField.getText());
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setX((Screen.getPrimary().getBounds().getWidth()/2)-400);
+            stage.setY(0);
+            stage.show();
+            log.info("Player1's name is set to {}, Player2's name is set to {}, loading game scene.",
+                    player1TextField.getText(), player2TextField.getText());
+        }
+    }
+}
